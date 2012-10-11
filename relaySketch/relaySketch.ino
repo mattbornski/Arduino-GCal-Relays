913035456d846c677e4a70136af2296532938b7f
Matt Born <mattborn@milton.(none)>
2012-10-11 07:35:59 -0700

    Parse vCal events and HTTP headers to determine if we are currently within the timeframe of a scheduled event; if so, flip on relays.

--- relaySketch/relaySketch.ino
+++ relaySketch/relaySketch.ino
@@ -4,18 +4,20 @@
 #include <EthernetClient.h>
 #include <Dhcp.h>
 
+String CALENDAR_FEED_URL = "https://google.com/your calendars private vcal/ics format feed here";
+
 byte myMac[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x66 };
 IPAddress myIp(192, 168, 1, 66);
 IPAddress myDns(192, 168, 1, 1);
 
 EthernetClient client;
-char server[] = "www.arduino.cc";
+char server[] = "proxy.bornski.com";
 
 unsigned long lastConnectionTime = 0;          // last time you connected to the server, in milliseconds
 boolean lastConnected = false;                 // state of the connection last time through the main loop
-const unsigned long postingInterval = 60*1000;  // delay between updates, in milliseconds
+const unsigned long postingInterval = 10*1000;  // delay between updates, in milliseconds
 
-byte relayPin[4] = { 2, 3, 4, 5}; 
+byte relayPin[4] = { 2, 3, 4, 5};
 //D2 -> RELAY1
 //D3 -> RELAY2
 //D4 -> RELAY3
@@ -45,24 +47,59 @@ void setup() {
   }
 }
 
-void loop() {
-   
-  int i;
-  for (i = 0; i < 4; i++) {
-    digitalWrite(relayPin[i], HIGH);
-  }
-  delay(10000);
-  for (i = 0; i < 4; i++) {
-    digitalWrite(relayPin[i], LOW);
-  }
-  delay(10000);
-  
-  // if there's incoming data from the net connection.
-  // send it out the serial port.  This is for debugging
-  // purposes only:
+void loop() { 
   if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
+    byte relayStates[4] = { LOW, LOW, LOW, LOW };
+    String now = NULL;
+    String summary = NULL;
+    String dtstart = NULL;
+    String dtend = NULL;
+    String line = "";
+    while (char c = client.read()) {
+      line += c;
+      //Serial.print(c);
+      if (c == '\n') {
+        if (line.startsWith("END:VCALENDAR")) {
+          client.stop();
+          break;
+        } else if (now == NULL && line.startsWith("Date:")) {
+          now = parseHttpDate(line.substring(6, line.length() - 1));
+        } else if (line.startsWith("BEGIN:VEVENT")) {
+          summary = NULL;
+          dtstart = NULL;
+          dtend = NULL;
+        } else if (line.startsWith("END:VEVENT")) {
+          // Parse event
+          Serial.println("Parse event:");
+          Serial.println(summary);
+          Serial.println(dtstart);
+          Serial.println(dtend);
+          Serial.println("Time is now:");
+          Serial.println(now);
+          Serial.println("date >= dtstart?");
+          Serial.println((dtstart <= now ? "yes": "no"));
+          Serial.println("date < dtend?");
+          Serial.println((now < dtend ? "yes": "no"));
+          if (dtstart <= now && dtend > now) {
+            relayStates[0] = HIGH;
+            relayStates[1] = HIGH;
+            relayStates[2] = HIGH;
+            relayStates[3] = HIGH;
+          }
+        } else if (line.startsWith("DTSTART:")) {
+          dtstart = parseVCalDate(line.substring(8, line.length() - 1));
+        } else if (line.startsWith("DTEND:")) {
+          dtend = parseVCalDate(line.substring(6, line.length() - 1));
+        } else if (line.startsWith("SUMMARY:")) {
+          summary = line.substring(8, line.length() - 1);
+        }
+        line = "";
+      }
+    }
+    
+    for (int i = 0; i < 4; i++) {
+      digitalWrite(relayPin[i], relayStates[i]);
+    }
   }
 
   // if there's no net connection, but there was one last time
@@ -75,12 +112,70 @@ void loop() {
 
   // if you're not connected, and ten seconds have passed since
   // your last connection, then connect again and send data:
-  if(!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
+//  Serial.println(millis() - lastConnectionTime);
+//  Serial.println(client.connected());
+  if(!client.connected() && (((millis() - lastConnectionTime) > postingInterval) || (lastConnectionTime == 0))) {
     httpRequest();
   }
   // store the state of the connection for next time through
   // the loop:
   lastConnected = client.connected();
+}
+
+String parseVCalDate(String dt) {
+  return dt;
+}
+
+String parseHttpDate(String dt) {
+  // RFC 822 date format is
+  // DOM, [D]D MMM YYYY hh:mm:ss GMT
+  int prevSplit = dt.indexOf(' ') + 1;
+  int nextSplit = dt.indexOf(' ', prevSplit);
+  String DD = "0" + dt.substring(prevSplit, nextSplit);
+  DD = DD.substring(DD.length() - 2, DD.length());
+  prevSplit = nextSplit + 1;
+  nextSplit = dt.indexOf(' ', prevSplit);
+  String MM = dt.substring(prevSplit, nextSplit);
+  if (MM.equalsIgnoreCase("JAN")) {
+    MM = "01";
+  } else if (MM.equalsIgnoreCase("FEB")) {
+    MM = "02";
+  } else if (MM.equalsIgnoreCase("MAR")) {
+    MM = "03";
+  } else if (MM.equalsIgnoreCase("APR")) {
+    MM = "04";
+  } else if (MM.equalsIgnoreCase("MAY")) {
+    MM = "05";
+  } else if (MM.equalsIgnoreCase("JUN")) {
+    MM = "06";
+  } else if (MM.equalsIgnoreCase("JUL")) {
+    MM = "07";
+  } else if (MM.equalsIgnoreCase("AUG")) {
+    MM = "08";
+  } else if (MM.equalsIgnoreCase("SEP")) {
+    MM = "09";
+  } else if (MM.equalsIgnoreCase("OCT")) {
+    MM = "10";
+  } else if (MM.equalsIgnoreCase("NOV")) {
+    MM = "11";
+  } else if (MM.equalsIgnoreCase("DEC")) {
+    MM = "12";
+  }
+  prevSplit = nextSplit + 1;
+  nextSplit = dt.indexOf(' ', prevSplit);
+  String YY = dt.substring(prevSplit, nextSplit);
+  prevSplit = nextSplit + 1;
+  nextSplit = dt.indexOf(':', prevSplit);
+  String hh = dt.substring(prevSplit, nextSplit);
+  prevSplit = nextSplit + 1;
+  nextSplit = dt.indexOf(':', prevSplit);
+  String mm = dt.substring(prevSplit, nextSplit);
+  prevSplit = nextSplit + 1;
+  nextSplit = dt.indexOf(' ', prevSplit);
+  String ss = dt.substring(prevSplit, nextSplit);
+  String ret = YY + MM + DD + "T" + hh + mm + ss + "Z";
+  Serial.println("parsed " + ret);
+  return ret;
 }
 
 // this method makes a HTTP connection to the server:
@@ -89,8 +184,8 @@ void httpRequest() {
   if (client.connect(server, 80)) {
     Serial.println("connecting...");
     // send the HTTP GET request:
-    client.println("GET /latest.txt HTTP/1.1");
-    client.println("Host: www.arduino.cc");
+    client.println("GET /" + CALENDAR_FEED_URL + " HTTP/1.1");
+    client.println("Host: proxy.bornski.com");
     client.println("User-Agent: arduino-ethernet");
     client.println("Connection: close");
     client.println();
