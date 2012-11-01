dfc70b554ba58c6721cc0ba4b3dd860ccd328394
Matt Born <mattborn@milton.(none)>
2012-11-01 08:45:29 -0700

    change networking to try to avoid the continuous-fail state of not having closed the connection properly. Start trimming the string usage to try to avoid memory leaks.  TODO stop using string, entirely, since it causes sketch to crash and freeze after a few iterations.

--- relaySketch/relaySketch.ino
+++ relaySketch/relaySketch.ino
@@ -14,7 +14,6 @@ EthernetClient client;
 char server[] = "proxy.bornski.com";
 
 unsigned long lastConnectionTime = 0;          // last time you connected to the server, in milliseconds
-boolean lastConnected = false;                 // state of the connection last time through the main loop
 const unsigned long postingInterval = 10*1000;  // delay between updates, in milliseconds
 
 byte relayPin[4] = { 2, 3, 4, 5};
@@ -47,79 +46,80 @@ void setup() {
   }
 }
 
-void loop() { 
-  if (client.available()) {
-    byte relayStates[4] = { LOW, LOW, LOW, LOW };
-    String now = NULL;
-    String summary = NULL;
-    String dtstart = NULL;
-    String dtend = NULL;
-    String line = "";
-    while (char c = client.read()) {
-      line += c;
-      //Serial.print(c);
-      if (c == '\n') {
-        if (line.startsWith("END:VCALENDAR")) {
-          client.stop();
-          break;
-        } else if (now == NULL && line.startsWith("Date:")) {
-          now = parseHttpDate(line.substring(6, line.length() - 1));
-        } else if (line.startsWith("BEGIN:VEVENT")) {
-          summary = NULL;
-          dtstart = NULL;
-          dtend = NULL;
-        } else if (line.startsWith("END:VEVENT")) {
-          // Parse event
-          Serial.println("Parse event:");
-          Serial.println(summary);
-          Serial.println(dtstart);
-          Serial.println(dtend);
-          Serial.println("Time is now:");
-          Serial.println(now);
-          Serial.println("date >= dtstart?");
-          Serial.println((dtstart <= now ? "yes": "no"));
-          Serial.println("date < dtend?");
-          Serial.println((now < dtend ? "yes": "no"));
-          if (dtstart <= now && dtend > now) {
-            relayStates[0] = HIGH;
-            relayStates[1] = HIGH;
-            relayStates[2] = HIGH;
-            relayStates[3] = HIGH;
-          }
-        } else if (line.startsWith("DTSTART:")) {
-          dtstart = parseVCalDate(line.substring(8, line.length() - 1));
-        } else if (line.startsWith("DTEND:")) {
-          dtend = parseVCalDate(line.substring(6, line.length() - 1));
-        } else if (line.startsWith("SUMMARY:")) {
-          summary = line.substring(8, line.length() - 1);
-        }
-        line = "";
-      }
-    }
+void loop() {
+  // If the designated interval has passed since you last connected, then connect again.
+  if (((millis() - lastConnectionTime) > postingInterval) || (lastConnectionTime == 0)) {
+    httpRequest();
+    
+    byte relayStates[4] = {LOW, LOW, LOW, LOW};
+    parseResponse(relayStates);
     
     for (int i = 0; i < 4; i++) {
       digitalWrite(relayPin[i], relayStates[i]);
     }
-  }
-
-  // if there's no net connection, but there was one last time
-  // through the loop, then stop the client:
-  if (!client.connected() && lastConnected) {
-    Serial.println();
-    Serial.println("disconnecting.");
+    
+    // Flush all remaining data
+    while (client.connected()) {
+      while (client.available()) {
+        client.read();
+      }
+    }
+    // Terminate our half of the connection.
     client.stop();
   }
+}
 
-  // if you're not connected, and ten seconds have passed since
-  // your last connection, then connect again and send data:
-//  Serial.println(millis() - lastConnectionTime);
-//  Serial.println(client.connected());
-  if(!client.connected() && (((millis() - lastConnectionTime) > postingInterval) || (lastConnectionTime == 0))) {
-    httpRequest();
+void parseResponse(byte relayStates[]) {
+  String now = NULL;
+  String summary = NULL;
+  String dtstart = NULL;
+  String dtend = NULL;
+  String line = "";
+  
+  // The server will close it's side of the connection when it is finished transferring data.
+  while (client.connected()) {
+    // It's possible that there is still data to come, but it is not yet ready to read.
+    while (client.available()) {
+      while (char c = client.read()) {
+        if (c == '\n') {
+          if (line.startsWith("END:VCALENDAR")) {
+            return;
+          } else if (now == NULL && line.startsWith("Date:")) {
+            now = parseHttpDate(line.substring(6, line.length() - 1));
+          } else if (line.startsWith("BEGIN:VEVENT")) {
+            summary = NULL;
+            dtstart = NULL;
+            dtend = NULL;
+          } else if (line.startsWith("END:VEVENT")) {
+            // Parse event
+            int zone = -1;
+            if (summary.indexOf("zone0") != -1) {
+              zone = 0;
+            } else if (summary.indexOf("zone1") != -1) {
+              zone = 1;
+            } else if (summary.indexOf("zone2") != -1) {
+              zone = 2;
+            } else if (summary.indexOf("zone3") != -1) {
+              zone = 3;
+            }
+            
+            if (zone != -1 && dtstart <= now && dtend > now) {
+              relayStates[zone] = HIGH;
+            }
+          } else if (line.startsWith("DTSTART:")) {
+            dtstart = parseVCalDate(line.substring(8, line.length() - 1));
+          } else if (line.startsWith("DTEND:")) {
+            dtend = parseVCalDate(line.substring(6, line.length() - 1));
+          } else if (line.startsWith("SUMMARY:")) {
+            summary = line.substring(8, line.length() - 1);
+          }
+          line = "";
+        } else {
+          line += c;
+        }
+      }
+    }
   }
-  // store the state of the connection for next time through
-  // the loop:
-  lastConnected = client.connected();
 }
 
 String parseVCalDate(String dt) {
@@ -174,15 +174,14 @@ String parseHttpDate(String dt) {
   nextSplit = dt.indexOf(' ', prevSplit);
   String ss = dt.substring(prevSplit, nextSplit);
   String ret = YY + MM + DD + "T" + hh + mm + ss + "Z";
-  Serial.println("parsed " + ret);
   return ret;
 }
 
 // this method makes a HTTP connection to the server:
 void httpRequest() {
+  Serial.println("connecting...");
   // if there's a successful connection:
   if (client.connect(server, 80)) {
-    Serial.println("connecting...");
     // send the HTTP GET request:
     client.println("GET /" + CALENDAR_FEED_URL + " HTTP/1.1");
     client.println("Host: proxy.bornski.com");
@@ -192,11 +191,10 @@ void httpRequest() {
 
     // note the time that the connection was made:
     lastConnectionTime = millis();
-  } 
-  else {
+    Serial.println("connected");
+  } else {
     // if you couldn't make a connection:
-    Serial.println("connection failed");
-    Serial.println("disconnecting.");
+    Serial.println("Connection failed");
     client.stop();
   }
 }
